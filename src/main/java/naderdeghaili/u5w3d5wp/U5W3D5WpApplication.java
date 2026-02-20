0f1887e81e1518e76f065760195cc39c72fc3308
NAD3R <naderdeghaili@gmail.com>
2026-02-20 10:20:29 +0100

    initial commit

---
+++ src/main/java/naderdeghaili/u5w3d5wp/U5W3D5WpApplication.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w3d5wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5W3D5WpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(U5W3D5WpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/naderdeghaili/u5w3d5wp/U5W3D5WpApplicationTests.java
@@ -0,0 +1,13 @@
+package naderdeghaili.u5w3d5wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5W3D5WpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
